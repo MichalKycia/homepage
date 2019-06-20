d891516a76a05970de34fad421a0b270622ebf1d
Michal Kycia <mich.txt@gmail.com>
2019-06-20 22:33:38 +0200

    js_div

--- main.js
+++ main.js
@@ -5,3 +5,13 @@ console.log(name);
 console.log(age);
 
 console.log (`Nazywam sie ${name} i mam ${age} lat`)
+
+const scripttest = document.querySelector('.scripttest');
+
+console.log(scripttest)
+
+const paragraphs = document.querySelectorAll('p');
+
+console.log(paragraphs[0]);
+
+scripttest.innerHTML = 'a kuku panie kruku';
